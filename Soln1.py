cff70445e4ef441e05ca4419875a11ff3ef25503
AyushAggarwal929 <97230042+AyushAggarwal929@users.noreply.github.com>
2022-01-09 19:11:21 +0530

    Delete Soln1.py

--- Soln1.py
+++
@@ -1,9 +0,0 @@
-# ANSWER 1
-# taking input from user of three numbers
-a = int(input('Enter first number'))
-b = int(input('Enter second number'))
-c = int(input('Enter third number'))
-# finding average of three numbers
-x = (a + b + c) / 3
-# printing the average
-print(x)
